df1ee6e5af5a3fd8af3f8cb52e0311993da3893b
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2020-01-29 20:42:38 -0300

    Studing react fundamentals

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: [],
+    };
+
+    this.getDataAxios();
+  }
+
+  async getDataAxios() {
+    const response = await axios.get("https://api.rawg.io/api/games");
+    console.log(response.data.results);
+    this.setState({ games: response.data.results })
+  } 
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <ol>
+            {this.state.games.map(game =>
+              <li key={game.name}>{game.name}</li>
+            )}
+          </ol>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React aprenda react
+          </a>
+        </header>
+      </div>
+    );
+  }
+}  
+
 
 export default App;
