1794fe0540e3b2fe02b98d33fab929245fd27830
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2020-01-30 08:17:03 -0300

    More tests on react

--- src/App.js
+++ src/App.js
@@ -7,40 +7,49 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
+      usuario: '',
       games: [],
+      the_witcher: []
     };
 
+    this.handleChange = this.handleChange.bind(this);
     this.getDataAxios();
   }
 
   async getDataAxios() {
     const response = await axios.get("https://api.rawg.io/api/games");
-    console.log(response.data.results);
-    this.setState({ games: response.data.results })
+    const response2 = await axios.get("https://api.rawg.io/api/games/the-witcher-3-wild-hunt");
+
+    console.log(response2.data);
+    this.setState({ games: response.data.results, the_witcher: response2.data})
   } 
 
+  handleChange(e) {
+    this.setState({ usuario: e.target.value });
+  }
 
   render(){
+    let state = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            <p>Usuário: {state.usuario} </p>
+
+          <input value={state.usuario} 
+          onChange={this.handleChange}></input>
+
+          <div>
+            <span>Nome: {state.the_witcher.name}</span>
+            <div>Release: {state.the_witcher.released}</div>
+          </div>
+
           <ol>
-            {this.state.games.map(game =>
-              <li key={game.name}>{game.name}</li>
+            {
+              state.games.map(game =>
+              <li key={game.name}>{game.name}, rating: {game.rating}</li>
             )}
           </ol>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React aprenda react
-          </a>
         </header>
       </div>
     );
