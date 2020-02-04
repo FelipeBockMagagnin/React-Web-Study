bf3cc732b972817a6a17068c0ae3eae7c8b5621b
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2020-02-04 13:35:23 -0300

    routing and game page

--- src/App.js
+++ src/App.js
@@ -1,54 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import Navbar from './Navbar/Navbar'
-import GameList from './Games/GameList'
+import Navbar from './Navbar/Navbar';
+import GameList from './Games/GameList';
+import GamePage from './Games/GamePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      usuario: '',
-      games: [],
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.getDataAxios();
-  }
-
   render() {
-    let state = this.state;
     return (
-      <div className="App">
-        <Navbar />
+      <Router>
+        <div className="App">
+          <Navbar />
 
-        <div className="container">
-          <div className="form-row">
-            <div className="col-md-6">
-              <p>Usuário: {state.usuario} </p>
-              <input className='form-control' value={state.usuario} onChange={this.handleChange}></input>
-            </div>
-
-            <div className="col-md-6">
-              <GameList/>
-            </div>
+          <div>
+            <Switch>
+              <Route path="/game/:id" component={GamePage} />
+              <Route path="/" component={GameList} />
+            </Switch>
           </div>
+
         </div>
-      </div>
+      </Router>
+
     );
   }
-
-  async getDataAxios() {
-    const response = await axios.get("https://api.rawg.io/api/games");
-    console.log(response);
-
-    this.setState({ games: response.data.results })
-  }
-
-  handleChange(e) {
-    this.setState({ usuario: e.target.value });
-  }
 }
-
 
 export default App;
