ec630915ced3de2aebbf46a67d67d97632fe4cbf
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2020-02-07 13:15:14 -0300

    load more games function

--- src/Games/GameList.js
+++ src/Games/GameList.js
@@ -5,24 +5,38 @@ import GameItem from './GameItem';
 class GameList extends Component {
     constructor(props) {
         super(props);
-        this.gameData();
+        this.loadGames('https://api.rawg.io/api/games');
 
         this.state = {
             games: [],
+            nextGamesPost: "",
+            gamesLength: 0
         };
+
+        this.loadMoreGamesClick = this.loadMoreGamesClick.bind(this);
     };
+
+    loadMoreGamesClick() {
+        this.loadGames(this.state.nextGamesPost);
+    }
 
     render() {
-        return ( <div>{
-            this.state.games.map(game =>
-                <GameItem key={game.id} data={game} />
-        )}</div>);
+        return ( 
+            <div>
+                { this.state.games.map(game =>
+                    <GameItem key={game.id} data={game} />
+                )}
+                <button className="load-more-float-button" onClick={this.loadMoreGamesClick}>Load More: {this.state.gamesLength}</button>
+            </div>
+            
+        );
     };
 
-    async gameData() {
-        const data = await axios.get("https://api.rawg.io/api/games");
-
-        this.setState({ games: data.data.results })
+    async loadGames(url){
+        const data = await axios.get(url);
+        console.log(data);
+        this.setState({ games: this.state.games.concat(data.data.results), nextGamesPost: data.data.next })
+        this.setState({gamesLength: this.state.games.length})
     }
 }
 
