1b1a8dddf3ba36c22e05a1a77c421e135be9bcb3
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2020-02-08 18:09:11 -0300

    infine scroll in the front page

--- src/Games/GameList.js
+++ src/Games/GameList.js
@@ -1,42 +1,68 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import GameItem from './GameItem';
+import Loader from '../Loader';
 
 class GameList extends Component {
     constructor(props) {
         super(props);
-        this.loadGames('https://api.rawg.io/api/games');
 
         this.state = {
             games: [],
             nextGamesPost: "",
-            gamesLength: 0
+            gamesLength: 0,
+            showGameLoading: true
         };
 
-        this.loadMoreGamesClick = this.loadMoreGamesClick.bind(this);
-    };
+        this.loadGames('https://api.rawg.io/api/games');
+        this.handleScroll = this.handleScroll.bind(this);
+    };    
 
-    loadMoreGamesClick() {
-        this.loadGames(this.state.nextGamesPost);
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
     }
 
     render() {
-        return ( 
+        return (
             <div>
-                { this.state.games.map(game =>
+                {this.state.games.map(game =>
                     <GameItem key={game.id} data={game} />
                 )}
-                <button className="load-more-float-button" onClick={this.loadMoreGamesClick}>Load More: {this.state.gamesLength}</button>
+                {
+                    this.state.showGameLoading ? <Loader/> : null
+                }
+                <button className="load-more-float-button">Games Loaded: {this.state.gamesLength}</button>
             </div>
-            
         );
     };
 
-    async loadGames(url){
+    async loadGames(url) {
+        this.setState({showGameLoading: true})
         const data = await axios.get(url);
         console.log(data);
-        this.setState({ games: this.state.games.concat(data.data.results), nextGamesPost: data.data.next })
-        this.setState({gamesLength: this.state.games.length})
+        console.log(this.state.games);
+
+        this.setState({
+            games: this.state.games.concat(data.data.results),
+            nextGamesPost: data.data.next,
+            gamesLength: this.state.games.length,
+            showGameLoading: false
+        });
+    }
+
+    handleScroll() {
+        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const body = document.body;
+        const html = document.documentElement;
+        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        const windowBottom = windowHeight + window.pageYOffset;
+        if (windowBottom >= docHeight) {
+            this.loadGames(this.state.nextGamesPost);
+        }
     }
 }
 
