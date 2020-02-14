0a89bfd480c630ef52f756b366cf08d385fe3faf
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2020-02-14 08:02:06 -0300

    search games

--- src/Games/GameList.js
+++ src/Games/GameList.js
@@ -25,9 +25,17 @@ class GameList extends Component {
         window.removeEventListener("scroll", this.handleScroll);
     }
 
+    searchGames = (event) =>{
+        console.log(event);
+        this.loadGamesSearch('https://api.rawg.io/api/games', event.target.value);
+    }
+
     render() {
         return (
             <div className="game-list-panel">
+                <div className='col-md-6'>
+                    <input className="form-control" placeholder="search" onChange={this.searchGames}></input>
+                </div>
                 {this.state.games.map(game =>
                     <GameItem key={game.id} data={game} />
                 )}
@@ -38,11 +46,34 @@ class GameList extends Component {
         );
     };
 
+    async loadGamesSearch(url, searchstring){
+        this.setState({
+            games: [],
+            nextGamesPost: '',
+            gamesLength: 0,
+            showGameLoading: true
+        });
+        
+        const urlGet = url + '?search=' + searchstring;
+        const data = await axios.get(urlGet);
+
+        console.log(urlGet);
+        console.log('data', data);
+        console.log('games', this.state.games);
+
+        this.setState({
+            games: data.data.results,
+            nextGamesPost: data.data.next,
+            gamesLength: this.state.games.length + data.data.results.length,
+            showGameLoading: false
+        });
+    }
+
     async loadGames(url) {
         this.setState({showGameLoading: true})
         const data = await axios.get(url);
-        console.log(data);
-        console.log(this.state.games);
+        console.log('data', data);
+        console.log('state games', this.state.games);
 
         this.setState({
             games: this.state.games.concat(data.data.results),
